252c735613e91c881e2274f6411e4e7d366f613c
Hongbusi <q951686662@qq.com>
2022-01-22 01:31:32 +0800

    chore: optimization

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,13 +2,13 @@ import './styles/index.css'
 import { h, App } from 'vue'
 import { VPTheme } from '@vue/theme'
 import Banner from './components/Banner.vue'
-import PreferenceSwitch from './components/PreferenceSwitch.vue'
+// import PreferenceSwitch from './components/PreferenceSwitch.vue'
 import {
   preferComposition,
   preferSFC,
   filterHeadersByPreference
 } from './components/preferences'
-import SponsorsAside from './components/SponsorsAside.vue'
+// import SponsorsAside from './components/SponsorsAside.vue'
 
 export default Object.assign({}, VPTheme, {
   Layout: () => {
