4b74ebb9cae31ce994533c10a7e445707aee535d
Hongbusi <q951686662@qq.com>
2022-02-22 18:19:38 +0800

    feat: optimization

--- src/components/ListProjects.vue
+++ src/components/ListProjects.vue
@@ -17,13 +17,7 @@ defineProps<{ projects: Record<string, any[]> }>()
         :class="!item.link ? 'opacity-0 pointer-events-none h-0 -mt-8 -mb-4' : ''"
       >
         <div v-if="item.icon" class="pt-2 pr-5">
-          <Slidev v-if="item.icon === 'slidev'" class="text-4xl opacity-50" />
-          <VueUse v-else-if="item.icon === 'vueuse'" class="text-4xl opacity-50" />
-          <VueReactivity v-else-if="item.icon === 'vue-reactivity'" class="text-4xl opacity-50" />
-          <VueDemi v-else-if="item.icon === 'vue-demi'" class="text-4xl opacity-50" />
-          <Unocss v-else-if="item.icon === 'unocss'" class="text-4xl opacity-50" />
-          <Vitest v-else-if="item.icon === 'vitest'" class="text-4xl opacity-50" />
-          <div v-else class="text-3xl opacity-50" :class="item.icon || 'i-carbon-unknown'" />
+          <div class="text-3xl opacity-50" :class="item.icon || 'i-carbon-unknown'" />
         </div>
         <div class="flex-auto">
           <div cla ss="text-normal">{{ item.name }}</div>
@@ -34,14 +28,7 @@ defineProps<{ projects: Record<string, any[]> }>()
   </template>
   <div class="markdown pb5">
     <p op75>
-      <em>
-        Thanks for getting intersted in my works! If like them or find them useful, consider
-        &nbsp;<a
-          href="https://github.com/sponsors/antfu"
-          target="_blank"
-          rel="nofollow noopener noreferrer"
-        >sponsoring me</a>&nbsp;to support me keeping them sustainable. Cheers! :)
-      </em>
+      <em>Thanks for getting intersted in my works!</em>
     </p>
   </div>
 </template>
