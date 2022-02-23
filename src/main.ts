419aacf0460a42e34e3f685734b9246d81b9a1bf
Hongbusi <q951686662@qq.com>
2022-02-23 18:09:39 +0800

    feat: optimization

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import '@unocss/reset/tailwind.css'
 import 'uno.css'
+import './styles/main.css'
+import './styles/prose.css'
+import './styles/markdown.css'
+
 import App from './App.vue'
 
 const app = createApp(App)
