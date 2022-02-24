ddada3759a376607d5bef1b6b4476405f2458846
洪布斯 <q951686662@qq.com>
2022-02-24 13:58:42 +0800

    Create ViewSourceCode.vue

---
+++ src/components/ViewSourceCode.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{ link: string }>()
+</script>
+
+<template>
+  <span>
+    <bi-code-slash class="inline-block opacity-70" />
+    <a
+      class="opacity-50 ml-1"
+      :href="link"
+      target="_blank"
+    >View source code</a>
+  </span>
+</template>
