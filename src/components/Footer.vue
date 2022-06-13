c0e62b23e35dcf988bf38da1e07507c85ff05ea9
Hongbusi <q951686662@qq.com>
2022-06-13 11:19:21 +0800

    chore: update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <div class="mt-10 mb-6 prose m-auto opacity-50 flex flex-col">
     <span class="text-sm">Copyright © 2021-present Hongbusi</span>
     <span class="text-sm mt-4">
-      <a href="https://beian.miit.gov.cn/#/Integrated/index" target="_blank">
+      <a href="https://beian.miit.gov.cn" target="_blank">
         浙ICP备2022017304号-1
       </a>
     </span>
