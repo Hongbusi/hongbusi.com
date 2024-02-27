b1eb17a0a40f813acce8d13b3ebabc22f6d0554c
Hongbusi <coderhbs@gmail.com>
2024-02-27 14:10:05 +0800

    feat: support search

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -3,12 +3,12 @@ import Link from 'next/link'
 import { siteConfig } from '@/config/site'
 import { cn } from '@/lib/utils'
 
-// import { CommandMenu } from "@/components/command-menu"
 import { Icons } from '@/components/icons'
 import { MainNav } from '@/components/main-nav'
 import { MobileNav } from '@/components/mobile-nav'
 import { ModeToggle } from '@/components/mode-toggle'
 import { buttonVariants } from '@/components/ui/button'
+import { CommandMenu } from '@/components/command-menu'
 
 export function SiteHeader() {
   return (
@@ -17,9 +17,9 @@ export function SiteHeader() {
         <MainNav />
         <MobileNav />
         <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
-          {/* <div className="w-full flex-1 md:w-auto md:flex-none">
+          <div className="w-full flex-1 md:w-auto md:flex-none">
             <CommandMenu />
-          </div> */}
+          </div>
           <nav className="flex items-center">
             <Link
               href={siteConfig.links.github}
--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,10 @@ module.exports = antfu({
   react: true,
   rules: {
     'node/prefer-global/process': 'off',
+    'react/prop-types': 'off',
+    'react/no-unknown-property': 'off',
     'react-refresh/only-export-components': 'off',
+    'no-use-before-define': 'off',
+    'ts/no-use-before-define': 'off',
   },
 })
