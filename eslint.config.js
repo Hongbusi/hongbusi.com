123d7320ab520c312730e7bfde8c495bb2c5b2f0
Hongbusi <coderhbs@gmail.com>
2024-03-03 02:02:01 +0800

    chore: add particles

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,8 @@ module.exports = antfu({
   react: true,
   rules: {
     'ts/no-require-imports': 'off',
+    'ts/no-use-before-define': 'off',
     'react-refresh/only-export-components': 'off',
+    'react-hooks/exhaustive-deps': 'off',
   },
 })
